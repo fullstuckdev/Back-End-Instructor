bd574e6b0a38b08e5e1b73aa3388f79c1487d29d
Taufik <informatikataufik@gmail.com>
2022-12-30 17:56:29 +0700

    POST & GET

--- controllers/DevControllers.js
+++ controllers/DevControllers.js
@@ -5,6 +5,16 @@ export const getData = async (req, res) => {
         const data = await Developer.find()
         res.json(data)
     } catch(error) {
-        res.status(500).json({message: error.message})
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const postData = async (req, res) => {
+    const dataDev = new Developer(req.body)
+    try {
+        const postData = await dataDev.save()
+        res.status(201).json(postData)
+    }catch(error) {
+        res.status(400).json({message: error.message})
     }
 }
--- routes/DevRoute.js
+++ routes/DevRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {getData} from "../controllers/DevControllers.js"
+import {getData, postData} from "../controllers/DevControllers.js"
 
 const router = express.Router()
 
 router.get('/developer', getData)
+router.post('/developer', postData)
 
 export default router
